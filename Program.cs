49a29f6c411b3d8ab05ab1f8e3d66fc7801a6f81
Vadim Smirnov <vadimsmirnov01@inbox.ru>
2021-02-11 15:25:42 +0300

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+
+namespace helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string eS = "Hello world";
+            int ei = 50;
+            bool eb = true; //true/false
+            char ech = 'n'; //только 1 символ
+            Console.WriteLine(eS);
+            string[] hellos = { "Hallo", "Hello", "Salve", "Ciao" };
+            Console.WriteLine(hellos[0]);
+            Console.WriteLine(hellos[3]);
+
+            Console.WriteLine(eS);
+            //for (int i = 0; i < hellos.Length; i++)
+            int i = 0;
+            foreach (string hello in hellos)
+            {
+                Console.WriteLine(hello);
+                i++;
+                Console.WriteLine("You have continued " + i + " times");
+            }
+
+            ech = Console.ReadLine()[0];
+            switch (ech)
+            {
+                case 'n':
+                    Console.WriteLine("Its N");
+                    break;
+
+                case 't':
+                    Console.WriteLine("Its T");
+                    break;
+
+                case 'b':
+                    Console.WriteLine("Its B");
+                    break;
+
+                default:
+                    Console.WriteLine("It is not any of known symbols.");
+                    break;
+
+            }
+            /*
+        if (Console.ReadKey().Key == ConsoleKey.N)
+        {
+            Console.WriteLine("\nВы продолжили.");
+            Console.ReadKey();
+        }
+        else
+        {
+            Console.WriteLine("\nБла бла бла");
+            Console.ReadKey();
+        }
+    }
+            */
+
+        }
+    }
+}
